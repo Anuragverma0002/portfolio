1fd743ae4713a840d470ab19995f17db58debc41
Anuragverma0002 <vermaanurag0209@gmail.com>
2025-08-04 00:21:59 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  body.classList.toggle("light");
+  toggleBtn.textContent = body.classList.contains("dark") ? "☀️" : "🌙";
+});
